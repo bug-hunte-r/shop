26aa3f3fc581f6a3aade3cef2ec23ae0c922b6e4
shayan <shayanjafarnejad75@gmail.com>
2025-08-25 10:30:49 +0330

    develop api for signup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,41 @@
-import { Controller, Get, Post, Body} from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { SignupDto } from './signup-dto/signup-dto';
+import type { Request, Response } from 'express';
+import { generateToken } from 'configs/auth';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
+
+  @Post('Signup')
+  async SignupUser(@Body() signupDto: SignupDto, @Req() req: Request, @Res() res: Response) {
+
+    try {
+
+      const newUser = await this.usersService.SignupUser(signupDto)
+
+      const token = generateToken({ username: signupDto.username })
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        path: '/',
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60 * 48
+      })
+
+      res.status(201).json({
+        message: newUser
+      })
+
+    } catch (error) {
+
+      res.status(error.getStatus ? error.getStatus() : 500).json({
+        message: error.message
+      })
+
+    }
+  }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,30 @@
 import { Injectable, BadRequestException, ConflictException } from '@nestjs/common';
 import { SignupDto } from './signup-dto/signup-dto';
 import User from 'models/user';
+import { hasePassHandler } from 'configs/auth';
 
 @Injectable()
 export class UsersService {
 
+    async SignupUser(signupDto: SignupDto) {
 
+        if (!signupDto.username.trim() || !signupDto.mobile || !signupDto.password.trim()) {
+            throw new BadRequestException('Datas are not valid')
+        }
+
+        const isUsernameExist = await User.findOne({ username: signupDto.username })
+
+        if (isUsernameExist) {
+            throw new ConflictException('Username is already exist')
+        }
+
+        const hashedPass = await hasePassHandler(signupDto.password)
+
+        const users = await User.countDocuments()
+
+        await User.create({ ...signupDto, password: hashedPass, role: users === 0 ? 'USER' : 'ADMIN' })
+
+        return 'User signuped successfully'
+    }
 
 }
