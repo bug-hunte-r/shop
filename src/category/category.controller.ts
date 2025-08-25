0caa3740ef6d60fbfeda90ffedceb35f53bcd622
shayan <shayanjafarnejad75@gmail.com>
2025-08-25 13:41:26 +0330

    develop api for create new category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,27 @@
-import { Controller, Get, Post, Body} from '@nestjs/common';
+import { Controller, Get, Post, Body, Res } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { AddCategoryDto } from './add-category-dto/add-category-dto';
+import type { Response } from 'express';
 
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+  constructor(private readonly categoryService: CategoryService) { }
 
+  @Post()
+  async addCategory(@Body() addCategoryDto: AddCategoryDto, @Res() res: Response) {
+    try {
+
+      res.status(201).json({
+        message: await this.categoryService.addCategory(addCategoryDto)
+      })
+
+    } catch (error) {
+
+      res.status(error.getStatus ? error.getStatus() : 500).json({
+        message: error.message
+      })
+
+    }
+    
+  }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, ConflictException } from '@nestjs/common';
+import { AddCategoryDto } from './add-category-dto/add-category-dto';
+import Category from 'models/category';
 @Injectable()
 export class CategoryService {
+
+    async addCategory(addCategoryDto: AddCategoryDto) {
+
+        if (!addCategoryDto.title.trim()) {
+            throw new BadRequestException('Data is not valid')
+        }
+        
+        const isCategoryExist = await Category.findOne({ title: addCategoryDto.title })
+        
+        if (isCategoryExist) {
+            throw new ConflictException('This category is already exist')
+        }
+
+        await Category.create({ ...addCategoryDto })
+
+        return 'Category add successfully'
+    }
 }
