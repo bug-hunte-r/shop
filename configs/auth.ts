fbc1064a4985a6b880fa4ce840320f69e989f072
shayan <shayanjafarnejad75@gmail.com>
2025-08-25 10:13:08 +0330

    develop logic for generate token

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,8 +1,16 @@
 import { hash } from "bcryptjs"
+import { sign } from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const hasePassHandler = async (password: string) => {
     const hashedPass = hash(password, 12)
     return hashedPass
 }
 
-export {hasePassHandler}
+const generateToken = ({ ...data }) => {
+    const token = sign(data, process.env.privateKey)
+    return token
+}
+
+export { hasePassHandler, generateToken }
