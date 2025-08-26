01273051a91780c67c316e01258447b437dc9e6b
shayan <shayanjafarnejad75@gmail.com>
2025-08-26 10:51:44 +0330

    develop dto for comments

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCommentDto {}
+import { IsString, MinLength } from "class-validator";
+import mongoose from "mongoose";
+
+export class CreateCommentDto {
+
+    @IsString()
+    @MinLength(5)
+    body: string;
+
+    user: mongoose.Types.ObjectId;
+
+    product: mongoose.Types.ObjectId;
+}
