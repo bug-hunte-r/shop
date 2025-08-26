dd7213efe2a5e3fec41cc97138024b4b03b90e2e
shayan <shayanjafarnejad75@gmail.com>
2025-08-26 22:37:43 +0330

    develop schema for users address

---
+++ models/address.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+
+    province: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: Number,
+        required: true
+    },
+
+    neighborhood: {
+        type: String,
+        required: true
+    },
+
+    street: {
+        type: String,
+        required: true
+    },
+
+    user: {
+        required: true,
+        ref: 'User',
+        type: mongoose.Types.ObjectId
+    }
+
+})
+
+const Address = mongoose.models.Address || mongoose.model('Address', addressSchema)
+
+export default Address
