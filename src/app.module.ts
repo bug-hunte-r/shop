fb9dba6c18145a9f360aeb49d09b56f3d0e663af
shayan <shayanjafarnejad75@gmail.com>
2025-08-29 16:56:37 +0330

    create resource for users orders

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CategoryModule } from './category/category.module';
 import { ProductsModule } from './products/products.module';
 import { CommentsModule } from './comments/comments.module';
 import { AddressModule } from './address/address.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [UsersModule, CategoryModule, ProductsModule, CommentsModule, AddressModule],
+  imports: [UsersModule, CategoryModule, ProductsModule, CommentsModule, AddressModule, OrderModule],
   controllers: [],
   providers: [UsersService],
 })
