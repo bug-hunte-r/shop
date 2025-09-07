2a416aea00de5de5e9f17d04f9f6b6cd7a58cddc
shayan <shayanjafarnejad75@gmail.com>
2025-09-07 11:01:12 +0330

    develop dto for tickets

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,1 +1,17 @@
-export class CreateTicketDto {}
+import { IsString, MinLength } from "class-validator";
+import mongoose from "mongoose";
+
+export class CreateTicketDto {
+
+    user: mongoose.Types.ObjectId;
+
+    @IsString()
+    @MinLength(3)
+    subject: String;
+    
+    @IsString()
+    @MinLength(5)
+    body: String;
+
+    replyTo: mongoose.Types.ObjectId
+}
