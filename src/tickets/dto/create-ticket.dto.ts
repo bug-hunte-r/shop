3edf832ba2af1a0288499234d6dff89406c7df09
shayan <shayanjafarnejad75@gmail.com>
2025-09-10 17:08:42 +0330

    update the tickets dto and model

---
+++ src/tickets/dto/create-ticket.dto.ts
@@ -0,0 +1,26 @@
+import { IsBoolean, IsString, MinLength } from "class-validator";
+import mongoose from "mongoose";
+import { TicketStatus } from "src/enum/ticketEnum";
+
+export class CreateTicketDto {
+
+    user: mongoose.Types.ObjectId;
+
+    @IsString()
+    @MinLength(3)
+    subject: String;
+
+    @IsString()
+    @MinLength(5)
+    body: String;
+
+    department: mongoose.Types.ObjectId;
+
+    status: TicketStatus
+
+    @IsBoolean()
+    hasAnswer: Boolean
+    
+    @IsBoolean()
+    isItAnswer: Boolean
+}
