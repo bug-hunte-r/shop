5ba64c2dbcf4426af96587a6e824326e55765100
shayan <shayanjafarnejad75@gmail.com>
2025-09-11 18:45:13 +0330

    creat dto for cart

--- src/cart/dto/create-cart.dto.ts
+++ src/cart/dto/create-cart.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCartDto {}
+import { IsInt, MinLength } from "class-validator";
+import mongoose from "mongoose";
+
+export class CreateCartDto {
+
+    user: mongoose.Types.ObjectId;
+
+    product: mongoose.Types.ObjectId;
+
+    @IsInt()
+    @MinLength(0)
+    count: Number
+}
